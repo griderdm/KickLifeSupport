5d66721181ee5fe745f3e4c20d281831ed30c4db
griderd <woodywood245@yahoo.com>
2025-12-05 00:00:35 -0800

    Removed ignored items.

--- debugfilter.ps1
+++
@@ -1,4 +0,0 @@
-# Get content, filter for the string, and write to new file
-Get-Content .\ksp.log | Where-Object { $_.Contains("[KICKLS]") } | Set-Content .\KickLifeSupportDebug.log
-
-Write-Host "Done. Check KickLifeSupportDebug.log"
